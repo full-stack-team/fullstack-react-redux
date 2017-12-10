0712f8e7428913c9f0b33e9e483a1e9c2db27206
KrishnarajLoganathan <kperchla@sapient.com>
2017-12-10 22:31:42 +0530

    changes

--- src/reducers/tradeReducer.js
+++ src/reducers/tradeReducer.js
@@ -5,6 +5,18 @@ export default function tradeReducer(state = initialState.trades, action) {
   switch (action.type) {
     case types.LOAD_TRADES:
       return action.trades;
+    
+    case types.CREATE_TRADES:
+      return  [
+        ...state,
+        Object.assign({}, action.trade)
+      ];
+
+    case types.UPDATE_TRADES:
+      return [
+        ...state.filter(course => course.id !== action.course.id),
+        Object.assign({}, action.trade)
+      ];
 
     default:
       return state;
